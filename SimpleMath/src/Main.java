3fce0aae7059739f0eef516d79725543eced0557
Duc Luu <luuduc34@hotmail.com>
2023-06-02 14:03:39 +0200

    feature/add-soustraction-function

--- SimpleMath/src/Main.java
+++ SimpleMath/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+    int a;
+    int b;
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(soustraction(7,3));
     }
+    public static int soustraction (int a, int b) {
+        return a - b;
+    }
+
 }
