f4c80c5ed605184c71c37f985796b5801282e3ce
Duc Luu <luuduc34@hotmail.com>
2023-06-02 14:38:13 +0200

    feature/SM-add-soustraction-function

--- SimpleMath/src/Main.java
+++ SimpleMath/src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
-    int a;
-    int b;
+
     public static void main(String[] args) {
         System.out.println("Bienvenue sur cette calculatrice!");
-        System.out.println(soustraction(7,3));
     }
 
     public static int addition (int a, int b){
