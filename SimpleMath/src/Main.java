110573611b566d320c789c33ec6a663a6b42c3bc
Mirodeon <mehdi.habaru@gmail.com>
2023-06-02 14:17:31 +0200

    feat(power): Add power function

--- SimpleMath/src/Main.java
+++ SimpleMath/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        powerFunction();
+    }
+
+    public static void powerFunction() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Donner un nombre entier qui sera élevé à sa propre puissance:");
+        String input = scan.nextLine();
+        if (isIntegerString(input)) {
+            int inputInt = Integer.parseInt(input);
+            System.out.println(input + " élevé à sa propre puissance est égal à " + Double.toString(Math.pow(inputInt, inputInt)).replaceAll(".0", "") + ".");
+        } else {
+            System.out.println("Vous n'avez pas entré un nombre entier, bye!");
+        }
+    }
+
+    public static boolean isIntegerString(String input) {
+        return input.matches("-?\\d+");
     }
 }
