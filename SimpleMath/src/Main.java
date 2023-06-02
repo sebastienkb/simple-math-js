1a6d7871e5efe4c47235bbb88fcfe6d3d4ed8a47
Bacariq <administration@bacariq.com>
2023-06-02 12:31:53 +0200

    feat:Add GetName

--- SimpleMath/src/Main.java
+++ SimpleMath/src/Main.java
@@ -1,5 +1,16 @@
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        GetPrenom();
     }
+    public void GetPrenom(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Quel est votre prénom : ");
+        String chaine = scanner.next();
+        System.out.print("Votre prénom est : "+chaine);
+    }
+
 }
